e3362e9c42d2a5734afd5be2c56c715931409948
Liana Lo <liana.lo@prezi.com>
2014-10-26 01:21:47 +0200

    JS "this" object keyword

--- 3 - Function Expressions, Closures, Hoisting, Objects, Prototypes/4.Objects/3.Using_Objects/1.using_objects.js
+++ 3 - Function Expressions, Closures, Hoisting, Objects, Prototypes/4.Objects/3.Using_Objects/1.using_objects.js
@@ -3,13 +3,25 @@ var rockSpearguns = {
   Pokepistol: {barbs: 4, weight: 8, heft: "shoulder"},
   Javelinjet: {barbs: 4, weight: 12, heft: "waist"},
   Firefork: {barbs: 6, weight: 8, heft: "overhand"},
-  "The Impaler": {barbs: 1, weight: 30, heft: "chest"}
-};
+  "The Impaler": {barbs: 1, weight: 30, heft: "chest"},
 
-function listGuns(guns) {
-	for (speargun in rockSpearguns) {
-		console.log("Behold! " + speargun + ", with " + rockSpearguns[speargun].heft + " heft!");
+  listGuns: function () {
+	for (var property in this) {
+		// console.log(property);
+		if (property.heft != undefined) {
+			console.log("Behold! " + property + ", with " + rockSpearguns[propety].heft + " heft!");
+		}
 	}
 }
 
-listGuns(rockSpearguns);
+};
+
+// function listGuns(guns) {
+// 	for (var speargun in rockSpearguns) {
+// 		console.log("Behold! " + speargun + ", with " + rockSpearguns[speargun].heft + " heft!");
+// 	}
+// }
+
+// listGuns(rockSpearguns);
+
+rockSpearguns.listGuns();
