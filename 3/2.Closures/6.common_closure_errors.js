0b7af78d1d07559d0919bee2493e10505dbc46f6
Liana Lo <liana.lo@prezi.com>
2014-10-25 21:34:40 +0200

    code before correcting for closure error

---
+++ 3/2.Closures/6.common_closure_errors.js
@@ -0,0 +1,18 @@
+function assignLaser( shark, sharkList ){
+  var stationAssignment;
+  for(var i = 0; i<sharkList.length; i++){
+    if(shark == sharkList[i]){
+      stationAssignment = function(){
+        alert("Yo, " +
+              shark +
+              "!\n" +
+              "Visit underwater strapping station " +
+              i +
+              " for your sweet laser.\n" +
+              "'Bout to get real up in here."
+             );
+      };
+    }
+  }
+  return stationAssignment;
+}
