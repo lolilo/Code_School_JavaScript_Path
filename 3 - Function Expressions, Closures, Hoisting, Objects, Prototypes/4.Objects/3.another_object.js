c802842b3a4b96d401911478dd17d20c58708ea0
Liana Lo <liana.lo@prezi.com>
2014-10-25 23:40:37 +0200

    highest wattage logged

--- 3 - Function Expressions, Closures, Hoisting, Objects, Prototypes/4.Objects/3.another_object.js
+++ 3 - Function Expressions, Closures, Hoisting, Objects, Prototypes/4.Objects/3.another_object.js
@@ -5,3 +5,18 @@ var lighthouseRock = {
   capacity: 30,
   secretPassageTo: "Underwater Outpost"
 };
+
+delete lighthouseRock.bulbs;
+lighthouseRock.weaponBulbs = superBlinders;
+
+var highestWattage = 0;
+var bulbWithHighestWattage;
+for (var i = 0; i < superBlinders.length; i++) {
+	
+	if (superBlinders[i][1] > highestWattage) {
+		highestWattage = superBlinders[i][1];
+		bulbWithHighestWattage = superBlinders[i][0];
+	}
+}
+
+console.log(bulbWithHighestWattage);
