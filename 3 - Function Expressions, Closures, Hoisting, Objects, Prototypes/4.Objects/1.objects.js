6cc73de2eb8df7214f4f5f4853f1f56d036d081a
Liana Lo <liana.lo@prezi.com>
2014-10-25 23:23:26 +0200

    simple objects

--- 3 - Function Expressions, Closures, Hoisting, Objects, Prototypes/4.Objects/1.objects.js
+++ 3 - Function Expressions, Closures, Hoisting, Objects, Prototypes/4.Objects/1.objects.js
@@ -2,6 +2,24 @@
 
 // Mutiple ways to build Objects. First, look at "Object literal."
 
+var vehicle1 = {
+	"type": "Moterboat",
+	"capacity": 6, 
+	"storedAt": "Ammunition Depot"
+}
+
+var vehicle2 = {
+	"type": "Jet Ski",
+	"capacity": 1, 
+	"storedAt": "Reef Dock"
+}
 
 
+var vehicle3 = {
+	"type": "Submarine",
+	"capacity": 8, 
+	"storedAt": "Underwater Outpost"
+}
 
+console.log(vehicle1.capacity);
+console.log(vehicle1["capacity"]);
