991954d1e7764130ce9c3133bd79b35650f6e198
Liana Lo <liana.lo@prezi.com>
2014-10-26 21:15:31 +0100

    anonymous closures

--- best_practices/4.Modularity/2.AnonymouseClosures.js
+++ best_practices/4.Modularity/2.AnonymouseClosures.js
@@ -35,3 +35,29 @@ var CAVESOFCLARITY = (function(){
 })();
 
 
+// "To ensure that the number of bats is referenceable, 
+// build a public method called getBats that returns the private data. 
+// From your good work, the amount of bats will be retrievable, 
+// but the quantity will endure for the ages."
+
+var CAVESOFCLARITY = function () {
+          
+  var treasureChests = 3;
+  var bats = 345;
+
+  return {
+    stalactites: 4235,
+    stalagmites: 3924,
+    SECRET: {
+      openChest: function(){
+        treasureChests--;
+        alert("DA DADADA DAAAAAAA!");
+      }
+    }, 
+    getBats : function() {
+      return bats;
+    }
+  };
+}();
+
+
