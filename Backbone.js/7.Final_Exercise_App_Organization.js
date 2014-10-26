8d3e88ebdd670076b1a27481c517dff94356dcf9
Liana Lo <liana.lo@prezi.com>
2014-10-26 16:27:14 +0100

    comments

--- Backbone.js/7.Final_Exercise_App_Organization.js
+++ Backbone.js/7.Final_Exercise_App_Organization.js
@@ -1,3 +1,15 @@
+/*APP ORGANIZATION
+We are so close to wrapping up! Before we go, let's use our Router to clean up our app a bit.
+
+First, instead of assigning a Router class to AppRouter, just create the Router instance.
+
+Next, instead of passing in the appointmentList collection in initialize, create an instance of AppointmentList and assign it to this.appointmentList.
+
+Add a start function to the router to start our Backbone history with pushState on.
+
+Finally, call the router's start function from inside a jQuery ready function to ensure we don't start updating the DOM before it's ready.
+*/
+
 var AppRouter = new (Backbone.Router.extend({
   routes: { "appointments/:id": "show", "": "index" },
 
