ee91e6a79149ad1fb99728fb82153c17fdb58bb6
Liana Lo <liana.lo@prezi.com>
2014-10-26 23:51:30 +0100

    comments on request and response

--- Node.js/3.Streams/5.download_server.js
+++ Node.js/3.Streams/5.download_server.js
@@ -4,6 +4,14 @@
 // This time you can use 'pipe' to send the read stream into the response.
 
 // I don't understand what's what -- request vs response.
+// request is incoming data
+// response is outgoing data 
+
+// request is an instance of http.IncomingMessage
+// response is an instance of http.ServerResponse
+
+// pipe documentation, 
+// readable.pipe(destination, [options])
 
 var fs = require('fs');
 var http = require('http');
@@ -12,7 +20,8 @@ http.createServer(function(request, response) {
   response.writeHead(200, {'Content-Type': 'image/png'});
 
   var file = fs.createReadStream('icon.png');
-  file.pipe(response);
+  file.pipe(response); // pipe goes in the direction file -> response. server is sending out data/responding.
+	
 });
 
 
@@ -27,7 +36,7 @@ http.createServer(function(request, response) {
   var fileBytes = request.headers['content-length']; // find total bytes of target file
   var downloadedBytes = 0; // keep track of already downloaded bytes
   
-  file.pipe(response);
+  file.pipe(response); 
 
   request.on('data', function(chunk) {
     downloadedBytes += chunk.length; // update
