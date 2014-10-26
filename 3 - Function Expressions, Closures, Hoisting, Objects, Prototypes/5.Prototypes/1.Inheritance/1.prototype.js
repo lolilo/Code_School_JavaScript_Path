29aa52bfc8c841cdc72ffeb2a2c14727cf85bf46
Liana Lo <liana.lo@prezi.com>
2014-10-26 02:55:47 +0200

    editing the array prototype

---
+++ 3 - Function Expressions, Closures, Hoisting, Objects, Prototypes/5.Prototypes/1.Inheritance/1.prototype.js
@@ -0,0 +1,31 @@
+var canyonCows = [
+  {name: "Bessie", type: "cow", hadCalf: "Burt"},
+  {name: "Donald", type: "bull", hadCalf: null},
+  {name: "Esther", type: "calf", hadCalf: null},
+  {name: "Burt", type: "calf", hadCalf: null},
+  {name: "Sarah", type: "cow", hadCalf: "Esther"},
+  {name: "Samson", type: "bull", hadCalf: null},
+  {name: "Delilah", type: "cow", hadCalf: null}
+];
+
+// Array.prototype.countCattle = function (parameter) {
+// 	var count = 0;
+// 	for (var item in this) {
+// 		count++;
+// 		console.log(item); // this counts the countCattle function we just added
+// 	}
+// 	return count; 
+// }
+  
+
+Array.prototype.countCattle = function (parameter) {
+	var count = 0;
+	for (var i = 0; i < this.length; i++) {
+		if (this[i].type == parameter){
+			count++;
+		}
+	}
+	return count; 
+}
+  
+console.log(canyonCows.countCattle("cow"));
