25fafc22c60cdca3b658dfc87ee59f82ff52fc4f
Liana Lo <liana.lo@prezi.com>
2014-10-26 03:01:35 +0100

    renaming

---
+++ 3 - Function Expressions, Closures, Hoisting, Objects, Prototypes/5.Prototypes/2.Constructors/1.inheritance.js
@@ -0,0 +1,30 @@
+var genericPost =
+  {x: 0, y: 0, postNum: undefined,
+   connectionsTo: undefined,
+   sendRopeTo: function ( connectedPost ) {
+     if(this.connectionsTo == undefined){
+       var postArray = [ ];
+       postArray.push(connectedPost);
+       this.connectionsTo = postArray;
+     } else {
+       this.connectionsTo.push(connectedPost);
+     }
+   }
+  };
+
+var post1 = Object.create(genericPost);
+var post2 = Object.create(genericPost);
+
+post1.x = -2;
+post1.y = 4;
+post1.postNum = 1;
+
+post2.x = 5;
+post2.y = 1;
+post2.postNum = 2;
+
+// post1.connectionsTo(post2);
+// post2.connectionsTo(post1);
+
+post1.sendRopeTo(post2);
+post2.sendRopeTo(post1);
