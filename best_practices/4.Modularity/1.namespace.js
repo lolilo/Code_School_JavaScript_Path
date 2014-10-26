e065f8c42d393128be63ec1c3056b582fca18103
Liana Lo <liana.lo@prezi.com>
2014-10-26 18:55:34 +0100

    namespace

---
+++ best_practices/4.Modularity/1.namespace.js
@@ -0,0 +1,12 @@
+var SINGLE = {
+
+   stalactites : 4235,
+   stalagmites : 3924,
+   bats : 345,
+   treasureChests : 3,
+   
+   openChest : function() {
+   	this.treasureChests--;
+   	alert("DA DADADA DAAAAAAA!");	
+	}
+};
