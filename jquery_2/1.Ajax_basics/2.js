0336cc0eb85fd7959150afb2c71fadc7cd9f4316
Liana Lo <liana.lo@prezi.com>
2014-10-30 21:00:34 +0100

    1 finished

--- jquery_2/1.Ajax_basics/2.js
+++ jquery_2/1.Ajax_basics/2.js
@@ -1,44 +1,4 @@
-$(document).ready(function() {
-  $("#tour").on("click", "button", function() {
-  	$.ajax('/photos.html', {
-  		success: function(response) {
-  			$('.photos').html(response).fadeIn();
-  		}
-
-  	})
-  });
-});
-
-
-// shortcut using .get
-// $.get (url, success)
-
-$(document).ready(function() {
-  $("#tour").on("click", "button", function() {
-  	$.get('/photos.html', function(response) {
-  			$('.photos').html(response).fadeIn();
-  	})
-  });
-});
-
-// You've decided you only want to get photos of london. 
-// Use the data option of the $.ajax function to pass a location option. 
-// Get the location from the data-location on the #tour element
-// using the data method.
-
-$(document).ready(function() {
-  $("#tour").on("click", "button", function() {
-  	$.ajax('/photos.html', {
-  		success: function(response) {
-  			$('.photos').html(response).fadeIn();
-  		},
-		data: {
-			location: $("#tour").data('location')
-		}
-  	})
-  });
-});
-
+x
 // errors
 $(document).ready(function() {
   var el = $("#tour");
